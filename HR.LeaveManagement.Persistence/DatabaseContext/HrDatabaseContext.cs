15e8c485124a57cdda4ab38b3f8c9418cc5d16f2
Maivu <maivanphuongvu.dhsp@gmail.com>
2024-05-18 07:53:25 +0700

    add Persistence

--- HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
+++ HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -1,7 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore;
-namespace HR.LeaveManagement.Persistence.DbContext
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+namespace HR.LeaveManagement.Persistence.DatabaseContext
 {
-    public class HrDatabaseContext
+    public class HrDatabaseContext : DbContext
     {
+        public HrDatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        //public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        //{
+        //    foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+        //        .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        //    {
+        //        entry.Entity.DateModified = DateTime.Now;
+        //        entry.Entity.ModifiedBy = _userService.UserId;
+        //        if (entry.State == EntityState.Added)
+        //        {
+        //            entry.Entity.DateCreated = DateTime.Now;
+        //            entry.Entity.CreatedBy = _userService.UserId;
+        //        }
+        //    }
+
+        //    return base.SaveChangesAsync(cancellationToken);
+        //}
     }
 }
